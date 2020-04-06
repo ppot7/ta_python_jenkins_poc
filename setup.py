ccdc48f81e1ff1c81ec1925dec0f20a202b46527
ppot7 <ppot7@yahoo.com>
2020-04-06 12:53:41 -0600

    Committing new files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ta_python_jenkins_poc',
+    version='0.0.1',
+    packages=['test_package'],
+    url='github://ppot7/ta_python_jenkins_poc',
+    license='',
+    author='paul potocki',
+    author_email='paul.potocki@transamerica.com',
+    description='Test of CI/CD for Python package using Jenkins to S3'
+)
